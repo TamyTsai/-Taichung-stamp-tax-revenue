3ec6712435de31f2cb9f8528401cad6d6acc8f90
Tamy <tamy8677@gmail.com>
2024-05-17 17:11:42 +0800

    data show in page

--- scripts/script.js
+++ scripts/script.js
@@ -14,44 +14,32 @@ function getData() {
             // 對json回傳的promise物件 接 .then ，成功的話 內容 會被傳入 then中 函數的參數jsonData 中
             .then(jsonData=> {
                 console.log(jsonData);
-                renderChart(jsonData) //把傳過來的json內容 用 Chart.js渲染成 視覺化資料
+                getSourceAndRevenue(jsonData); //回傳 傳過來的json內容（物件陣列）
             })
 }
 
-// 用Chart.js渲染資料 的 函數
-function renderChart(data) {
+
+function getSourceAndRevenue(data) {
     let taxSource = data.map(par => par.欄位名稱)
     // taxSource為裝有稅收來源（銀行、農漁會...）的陣列
     // map為ES6方法，會去一一拜訪陣列中的元素，並對這些元素做某些操作，再把操作後的結果蒐集成新陣列
     // par => par.欄位名稱 為 function (par) {return par.market} 之ES6箭頭函數簡寫（par為隨便取的參數名稱，目的只是為了在程式碼區塊中 有一個 可以拿來 進行 存取屬性值方法（.屬性） 的 標的 而已）
-    // .欄位名稱 或['欄位名稱']可以取出 陣列中 物件 屬性「欄位名稱」 的 值
+    // .欄位名稱 或['欄位名稱']可以取出 陣列中 物件 屬性「欄位名稱」的值
     let taxRevenue = data.map(par => par.數值)
     // taxRevenue為裝有 來自各來源 的 稅收金額 的陣列
 
-    console.log(taxSource)
-    console.log(taxRevenue)
+    console.log(taxSource);
+    console.log(taxRevenue);
 
-    //
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: taxSource, 
-            datasets: [{
-                // label: '沒有', 
-                data: taxRevenue, //變數datas 裝有 由 prices物件陣列中 物件 屬性price的值 組成的 陣列
-                borderWidth: 1 //柱狀圖的邊框寬度
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true //縱軸從0開始
-                    }
-                }]
-            }
-        }
-    });
-
+    for (var i = 0; i < 33; i++) {
+        $(`#taxSource${i}`).html(taxSource[i]);
+        $(`#taxRevenue${i}`).html(taxRevenue[i]);
+    }
+    //以jQuery寫法抓取HTML元素，並更改其內容
+    //以ES6寫法串接變數與字串
 }
+
+
+
+
+
